219b811cf60e6f86a648cf895b0ed288af7fc9d3
EmilJohan <emil.molberg@hotmail.no>
2024-03-11 18:35:02 +0100

    Task B

--- src/main/java/no/hvl/dat110/broker/Storage.java
+++ src/main/java/no/hvl/dat110/broker/Storage.java
@@ -51,51 +51,41 @@ public class Storage {
 	}
 
 	public void addClientSession(String user, Connection connection) {
-
-		// TODO: add corresponding client session to the storage
-		// See ClientSession class
-		
-		throw new UnsupportedOperationException(TODO.method());
+		ClientSession session = new ClientSession(user,connection);
+		clients.put(user,session);
 		
 	}
 
 	public void removeClientSession(String user) {
-
-		// TODO: disconnet the client (user) 
-		// and remove client session for user from the storage
-		
-		throw new UnsupportedOperationException(TODO.method());
+		// disconnect and remove client session for user from the storage
+		ClientSession session = clients.remove(user);
+		session.disconnect();
 		
 	}
 
 	public void createTopic(String topic) {
-
-		// TODO: create topic in the storage
-
-		throw new UnsupportedOperationException(TODO.method());
+		// creates topic in the storage
+		subscriptions.put(topic, ConcurrentHashMap.newKeySet());
 	
 	}
 
 	public void deleteTopic(String topic) {
-
-		// TODO: delete topic from the storage
-
-		throw new UnsupportedOperationException(TODO.method());
+		// deletes topic in the storage
+		subscriptions.remove(topic);
 		
 	}
 
 	public void addSubscriber(String user, String topic) {
+		// add user as subscriber to the topic
+		Set<String> subscribers = subscriptions.get(topic);
+		subscribers.add(user);
 
-		// TODO: add the user as subscriber to the topic
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
 	}
 
 	public void removeSubscriber(String user, String topic) {
+		// remove user as subscriber to the topic
+		Set<String> subscribers = subscriptions.get(topic);
+		subscribers.remove(user);
 
-		// TODO: remove the user as subscriber to the topic
-
-		throw new UnsupportedOperationException(TODO.method());
 	}
 }
